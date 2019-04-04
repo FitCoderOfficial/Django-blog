a4683762b99205795692b10f71b4d980e361df33
Shin-Sae-byeok <toqur1219@naver.com>
2019-04-04 11:35:38 +0900

    feat:Add_Categorys_Wiget

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,7 +1,16 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger 
 from django.shortcuts import render
+from django.db.models import Count 
 from .models import Post
 from marketing.models import Signup
+
+def get_category_count():
+    queryset = Post \
+        .objects \
+        .values('category__title') \
+        .annotate(Count('category__title')) 
+    return queryset 
+
 
 def index(request):
     featured = Post.objects.filter(featured=True)
@@ -20,6 +29,7 @@ def index(request):
     return render(request, 'index.html', context)
 
 def blog(request):
+    category_count = get_category_count()
     most_recent = Post.objects.order_by('-timestamp')[:3]
     post_list = Post.objects.all()
     paginator = Paginator(post_list, 4)
@@ -34,7 +44,8 @@ def blog(request):
     context = {
         'queryset': paginated_queryset,
         'most_recent': most_recent,
-        'page_request_var': page_request_var
+        'page_request_var': page_request_var,
+        'category_count':category_count
     }
     return render(request, 'blog.html', context)
 
