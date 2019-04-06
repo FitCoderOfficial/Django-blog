3fdacda8bab30dd492edaf3074d3f0196a73ce73
Shin-Sae-byeok <toqur1219@naver.com>
2019-04-06 23:31:44 +0900

    feat:Add_comment_form

--- src/posts/models.py
+++ src/posts/models.py
@@ -19,6 +19,7 @@ class Category(models.Model):
     def __str__(self):
         return self.title
 
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     overview = models.TextField()
@@ -40,5 +41,14 @@ class Post(models.Model):
         return reverse("post-detail", kwargs={
             "id": self.id
             })
-    
+
+
+class Comment(models.Model):
+    user = models.OneToOneField(User,  on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=False)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username     
 
