b21872bf47ce9cfac02f1350de62f51283145fe1
Shin-Sae-byeok <toqur1219@naver.com>
2019-04-09 14:21:47 +0900

    feat:Add_create_menu

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -120,8 +120,6 @@ MEDIA_ROOT = os.path.join(VENV_PATH, 'media_root')
 
 
 TINYMCE_DEFAULT_CONFIG = {
-    'height': 360,
-    'width': 1120,
     'cleanup_on_startup': True,
     'custom_undo_redo_levels': 20,
     'selector': 'textarea',
